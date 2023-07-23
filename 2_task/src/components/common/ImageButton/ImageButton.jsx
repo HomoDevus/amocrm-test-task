c89bb4af40d5b52e1efe362a5b011f17ab5f8741
ItIsEternity <lavrovslava207@gmail.com>
2023-07-24 02:39:09 +0300

    Prettier

--- 2_task/src/components/common/ImageButton/ImageButton.jsx
+++ 2_task/src/components/common/ImageButton/ImageButton.jsx
@@ -1,7 +1,9 @@
 import styles from './ImageButton.module.css'
 
-export default function ImageButton({src, isSelected = false}) {
-    return (
-        <button className={`${styles.button} ${isSelected ? styles.selected : ''}`}><img src={src} /></button>
-    )
+export default function ImageButton({ src, isSelected = false }) {
+  return (
+    <button className={`${styles.button} ${isSelected ? styles.selected : ''}`}>
+      <img src={src} />
+    </button>
+  )
 }
