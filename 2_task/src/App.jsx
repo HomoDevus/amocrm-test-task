0f33876921c6a01f36235da78d4bd8786b50659b
ItIsEternity <lavrovslava207@gmail.com>
2023-07-24 00:54:44 +0300

    Social media links added to landing

--- 2_task/src/App.jsx
+++ 2_task/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import CharacterSection from './components/sections/CharacterSection/CharacterSection'
 import LandingSection from './components/sections/LandingSection/LandingSection'
 
 function App() {
   return (
     <div className='App'>
       <LandingSection />
+      <CharacterSection />
     </div>
   )
 }
--- 2_task/src/components/sections/LandingSection/LandingSection.jsx
+++ 2_task/src/components/sections/LandingSection/LandingSection.jsx
@@ -1,9 +1,9 @@
+import landingImage from '../../../static/images/landingImage.png'
 import logo from '../../../static/images/logo.svg'
 import Button from '../../common/Button/Button'
 import PreTitle from '../../common/PreTitle/PreTitle'
 import Cart from './Cart/Cart'
 import styles from './LandingSection.module.css'
-import landingImage from '../../../static/images/landingImage.png'
 
 export default function LandingSection() {
   return (
@@ -23,10 +23,16 @@ export default function LandingSection() {
         </div>
       </header>
       <div className={styles.contentContainer}>
+        <div className={styles.socialMediaContainer}>
+          <a>Facebook</a>
+          <a>Instagram</a>
+          <a>Twitch</a>
+        </div>
         <div className={styles.conent}>
           <PreTitle>FUTURE OF ESPORTS</PreTitle>
           <h1 className={styles.contentHeading}>
-            Unleash the Next <span className={styles.curvedUnderline}>Generation</span> of Gaming
+            Unleash the Next{' '}
+            <span className={styles.curvedUnderline}>Generation</span> of Gaming
           </h1>
           <h6 className={styles.contentDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec
@@ -37,7 +43,9 @@ export default function LandingSection() {
             <Button type='text'>View our team</Button>
           </div>
         </div>
-        <img src={landingImage} alt='Fortnite soldier caracter' />
+        <div className={styles.imageContainer}>
+          <img src={landingImage} alt='Fortnite soldier caracter' />
+        </div>
       </div>
     </div>
   )
